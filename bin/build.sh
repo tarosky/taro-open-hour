bc1bd01ac5c62cac2f2d22e9f48dfd3dbad3759d
fumikito <guy@hametuha.com>
2019-08-27 20:47:32 +0900

    close #7 fix build file.

--- bin/build.sh
+++ bin/build.sh
@@ -3,13 +3,19 @@
 set -e
 
 # Build files
-composer install --no-dev
+composer install --no-dev --prefer-dist
 npm install
 npm run package
 # Make Readme
 echo 'Generate readme.'
 curl -L https://raw.githubusercontent.com/fumikito/wp-readme/master/wp-readme.php | php
 # Remove files
+rm -rf .git
+rm -rf .gitignore
+rm -rf .travis.yml
+rm -rf .jshintrc
+rm -rf phpcs.ruleset.xml
+rm -rf phpunit.xml.dist
 rm -rf node_modules
 rm -rf tests
 rm -rf bin
